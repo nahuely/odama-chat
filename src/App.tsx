be1e1b7ef3bbfc6e3dbd7359f713bc19ec8ebb05
Nahuel Amendola <nahuely@users.noreply.github.com>
2023-07-20 11:05:10 -0300

    chat, config routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Chat from "./views/chat";
+import Config from "./views/config";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/chat",
+      element: <Chat />,
+    },
+    {
+      path: "/config",
+      element: <Config />,
+    },
+  ],
+  {}
+);
+
 function App() {
-  return <>App</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
