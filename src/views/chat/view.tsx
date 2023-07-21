8de4ffebc1f5804fd9d98ceed6fbb644305414ba
Nahuel Amendola <nahuely@users.noreply.github.com>
2023-07-20 23:08:50 -0300

    config wip

--- src/views/chat/view.tsx
+++ src/views/chat/view.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { GearIcon } from "@components/GearIcon";
-import { BackIcon } from "@components/BackIcon";
 import { NewChat } from "./NewChat";
 import { ChatListHistory } from "./ChatListHistory";
 import { Chat } from "./Chat";
@@ -8,11 +7,7 @@ import { Chat } from "./Chat";
 function View() {
   return (
     <div className="container mx-auto bg-slate-50 min-h-screen">
-      <nav className="bg-[#F97316] p-6 justify-between flex">
-        <Link className="border-2 border-white rounded p-1" to={`/chat`}>
-          <BackIcon />
-        </Link>
-
+      <nav className="bg-[#F97316] p-6 justify-end flex">
         <Link className="border-2 border-white rounded p-1" to={`/config`}>
           <GearIcon />
         </Link>
--- src/views/config/index.tsx
+++ src/views/config/index.tsx
@@ -1,5 +1,80 @@
-function Config() {
-  return <>Config</>;
+import { BackIcon } from "@/components/BackIcon";
+import { Link } from "react-router-dom";
+
+function View() {
+  return (
+    <div className="container mx-auto bg-slate-50 min-h-screen">
+      <nav className="bg-[#F97316] p-6 justify-between flex">
+        <Link className="border-2 border-white rounded p-1" to={`/chat`}>
+          <BackIcon />
+        </Link>
+      </nav>
+      <section className="p-6">
+        <div className="rounded-md bg-white p-6 drop-shadow-md">
+          <h3 className="text-xl text-slate-800 mb-3">Configuracion</h3>
+
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div>
+              <label className="block mb-2" htmlFor="prompt">
+                temperature
+              </label>
+              <input
+                id="prompt"
+                type="range"
+                min="0"
+                max="2"
+                step="0.1"
+                placeholder="Insertar prompt"
+                onChange={(e) => {
+                  // setNewPrompt(e.target.value);
+                }}
+                // value={newPrompt}
+                className="border-2 border-slate-400 rounded  p-2"
+              />
+            </div>
+            <div>
+              <label className="block mb-2" htmlFor="tokens">
+                max tokens
+              </label>
+              <input
+                id="tokens"
+                type="range"
+                min="0"
+                max="256"
+                step="1"
+                placeholder="Insertar prompt"
+              />
+            </div>
+            <div>
+              <label className="block mb-2" htmlFor="model">
+                model
+              </label>
+              <select
+                id="model"
+                className="border-2 border-slate-400 rounded  p-2"
+              >
+                <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+                <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+                <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
+              </select>
+            </div>
+            <div className="mt-6">
+              <button
+                type="submit"
+                className="bg-slate-800 text-white rounded p-2"
+              >
+                Guardar
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default Config;
+export default View;
